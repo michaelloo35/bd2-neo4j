6e90026985c45280a07cbc68ed9be94dec9f3824
michaelloo35 <michaelloo35@gmail.com>
2017-12-16 17:06:28 +0100

    Initial commit

---
+++ src/main/java/pl/edu/agh/bd/Main.java
@@ -0,0 +1,11 @@
+package pl.edu.agh.bd;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        GraphDatabase.createDatabase();
+    }
+
+}
